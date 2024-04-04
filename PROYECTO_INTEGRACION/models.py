716739e3d1d5eefc8025e5c89759e0ef9b56bede
CarlosFHV <al2202000693@azc.uam.mx>
2024-04-04 10:41:36 -0600

    Delete models.py

--- PROYECTO_INTEGRACION/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class textos_informacion(models.Model):
-    texto_original = models.TextField()
-    texto_procesado = models.TextField()
-    analisis_sentimiento = models.FloatField()  # Usamos FloatField para almacenar un número decimal
-    etiqueta = models.CharField(max_length=50)
-    rango_izq = models.FloatField()
-    rango_der = models.FloatField()
